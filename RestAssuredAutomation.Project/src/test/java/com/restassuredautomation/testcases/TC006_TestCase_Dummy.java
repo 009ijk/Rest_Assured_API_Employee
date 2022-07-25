40231b68805a27d7ce551c7654d2b0e6dd85a178
inderjeet.kumar_vvdn <inderjeet.kumar_vvdn@1480--CNM--GGN>
2022-07-25 16:13:34 +0530

    Edding 6th test case.....

---
+++ RestAssuredAutomation.Project/src/test/java/com/restassuredautomation/testcases/TC006_TestCase_Dummy.java
@@ -0,0 +1,12 @@
+package com.restassuredautomation.testcases;
+
+import org.testng.annotations.Test;
+
+public class TC006_TestCase_Dummy {
+
+	@Test
+	void checkDemo()
+	{
+		System.out.println("Nice to learn git and github ");
+	}
+}
